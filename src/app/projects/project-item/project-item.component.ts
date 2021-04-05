ec93d6e712f42093ef8c4da25ecf2bfe66d4ef48
brunnermanu <brunner.manu@hotmail.com>
2021-04-05 14:22:31 +0200

    add projects

---
+++ src/app/projects/project-item/project-item.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Project} from '../project.model';
+
+@Component({
+  selector: 'app-project-item',
+  templateUrl: './project-item.component.html',
+  styleUrls: ['./project-item.component.scss']
+})
+export class ProjectItemComponent implements OnInit {
+  @Input() project: Project;
+  @Input() index: number;
+  @Input() routerLinkActive: string;
+
+  ngOnInit() {
+  }
+
+}
